7c5773cf3b06c6458b4dc9d26c1a869729eacf9a
sanjaysingh24 <sanjubora84@gmail.com>
2025-07-31 14:29:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const port = 3000;
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to the server")
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
