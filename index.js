2f1ee67410bb2c526959aaa61bafe792e7d23cc1
sanjaysingh24 <sanjubora84@gmail.com>
2025-07-31 15:24:51 +0530

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("Welcome to the server")
 })
-
+//update something
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
